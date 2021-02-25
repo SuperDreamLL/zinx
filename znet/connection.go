02deff7b8e323583c8be48ff2a2e7957382867b5
mengchao <mengc@soulgame.com>
2021-02-25 18:44:21 +0800

    修复部分问题

--- znet/connection.go
+++ znet/connection.go
@@ -49,7 +49,7 @@ func NewConntion(server ziface.IServer, conn *net.TCPConn, connID uint32, msgHan
 		MsgHandler:  msgHandler,
 		msgChan:     make(chan []byte),
 		msgBuffChan: make(chan []byte, utils.GlobalObject.MaxMsgChanLen),
-		property:    make(map[string]interface{}),
+		property:    nil,
 	}
 
 	//将新创建的Conn添加到链接管理中
@@ -162,19 +162,19 @@ func (c *Connection) Start() {
 
 //停止连接，结束当前连接状态M
 func (c *Connection) Stop() {
-	fmt.Println("Conn Stop()...ConnID = ", c.ConnID)
-
 	c.Lock()
 	defer c.Unlock()
-
-	//如果用户注册了该链接的关闭回调业务，那么在此刻应该显示调用
-	c.TcpServer.CallOnConnStop(c)
 
 	//如果当前链接已经关闭
 	if c.isClosed == true {
 		return
 	}
 	c.isClosed = true
+
+	fmt.Println("Conn Stop()...ConnID = ", c.ConnID)
+
+	//如果用户注册了该链接的关闭回调业务，那么在此刻应该显示调用
+	c.TcpServer.CallOnConnStop(c)
 
 	// 关闭socket链接
 	c.Conn.Close()
@@ -252,6 +252,9 @@ func (c *Connection) SendBuffMsg(msgId uint16, data []byte) error {
 func (c *Connection) SetProperty(key string, value interface{}) {
 	c.propertyLock.Lock()
 	defer c.propertyLock.Unlock()
+	if c.property == nil {
+		c.property = make(map[string]interface{})
+	}
 
 	c.property[key] = value
 }
@@ -274,4 +277,9 @@ func (c *Connection) RemoveProperty(key string) {
 	defer c.propertyLock.Unlock()
 
 	delete(c.property, key)
+}
+
+//返回ctx，用于用户自定义的go程获取连接退出状态
+func (c *Connection) Context() context.Context {
+	return c.ctx
 }
